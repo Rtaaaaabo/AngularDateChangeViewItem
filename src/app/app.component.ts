4c24ddda1d0225b577443bb4b834e3af2580fabb
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-09-09 22:04:48 +0900

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dateChangeView';
+export class AppComponent implements OnInit {
+  title = '';
+  date = new Date();  // 現在日時を得る
+  message: string;
+
+  constructor() {
+    this.message = '';
+  }
+
+  ngOnInit() {
+    if (this.date.getHours() < 6) {
+      this.message = 'おやすみ';
+    } else if (this.date.getHours() < 11) {
+      this.message = 'おはよう';
+    } else if (this.date.getHours() < 17) {
+      this.message = 'こんにちわ！';
+    } else {
+      this.message = 'こんばんわ！';
+    }
+  }
+
 }
